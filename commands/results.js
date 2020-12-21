886c03b751b9270bfdff151ddf5a7598553f8d70
raoul.loth <raoul.loth@asmc.de>
2020-12-21 22:25:05 +0100

    Reworked !missing_results to also show duplicate results

--- commands/results.js
+++ commands/results.js
@@ -3,10 +3,10 @@ const isStaffMember = require('../utils/isStaffMember');
 const sendAlertMessage = require('../utils/sendAlertMessage');
 
 module.exports = {
-  name: 'missing_results',
-  description: 'Check for missing results on Lorenzi.',
+  name: 'results',
+  description: 'Check for duplicate and missing results on Lorenzi.',
   guildOnly: true,
-  aliases: ['unsubmitted_results'],
+  aliases: ['unsubmitted_results', 'missing_results', 'duplicate_results'],
   execute(message, args) {
     if (!isStaffMember(message.member) && !message.member.roles.cache.find((r) => r.name.toLowerCase() === config.roles.ranked_updater_role.toLowerCase())) {
       return sendAlertMessage(message.channel, 'You are not allowed to use this command.');
@@ -25,12 +25,22 @@ module.exports = {
 
     channel.messages.fetch({ limit }).then((messages) => {
       const missingResults = [];
+      const duplicates = [];
 
-      messages.forEach((m) => {
+      messages.sort((a, b) => a.id - b.id).forEach((m) => {
         const reaction = m.reactions.cache.find((r) => r.emoji.name === '✅');
         if (!reaction) {
           missingResults.push(m.id);
         }
+
+        messages.forEach((m2) => {
+          if (m.id !== m2.id && m.content === m2.content && !duplicates.find((d) => [m.id, m2.id].includes(d[0]) && [m.id, m2.id].includes(d[1]))) {
+            duplicates.push([
+              m.id,
+              m2.id,
+            ]);
+          }
+        });
       });
 
       if (missingResults.length > 0) {
@@ -38,6 +48,17 @@ module.exports = {
         sendAlertMessage(message.channel, `There are ${missingResults.length} unsubmitted results within the last ${limit} submissions:\n\n${missingResults.map(format).join('\n')}`, 'warning');
       } else {
         sendAlertMessage(message.channel, 'There are no unsubmitted results. Good job!', 'success');
+      }
+
+      if (duplicates.length > 0) {
+        const format = (d) => {
+          const baseUrl = `https://discord.com/channels/${config.main_guild}/${channel.id}`;
+          return `${baseUrl}/${d[0]} + ${baseUrl}/${d[1]}`;
+        };
+
+        sendAlertMessage(message.channel, `There are ${duplicates.length} duplicate results within the last ${limit} submissions:\n\n${duplicates.map(format).join('\n')}`, 'warning');
+      } else {
+        sendAlertMessage(message.channel, 'There are no duplicate results. Good job!', 'success');
       }
     });
   },
