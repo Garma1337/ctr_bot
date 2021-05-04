7161b01559eff94082a753dda95692efa771bd8b
raoul.loth <raoul.loth@asmc.de>
2021-05-05 00:30:23 +0200

    Added all modes to the Rank model to fix the wrong display of MMR in non-FFA modes

--- db/models/rank.js
+++ db/models/rank.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const {
   RACE_FFA,
+  RACE_DUOS,
+  RACE_3V3,
+  RACE_4V4,
   RACE_SURVIVAL,
   RACE_ITEMLESS_FFA,
+  RACE_ITEMLESS_DUOS,
+  RACE_ITEMLESS_3V3,
+  RACE_ITEMLESS_4V4,
+  BATTLE_1V1,
   BATTLE_FFA,
+  BATTLE_DUOS,
+  BATTLE_3V3,
+  BATTLE_4V4,
+  BATTLE_SURVIVAL,
 } = require('./lobby');
 
 const { Schema, model } = mongoose;
@@ -11,9 +22,20 @@ const { Schema, model } = mongoose;
 const Rank = new Schema({
   name: String,
   [RACE_FFA]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_DUOS]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_3V3]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_4V4]: { rank: Number, position: Number, lastActivity: Number },
   [RACE_SURVIVAL]: { rank: Number, position: Number, lastActivity: Number },
   [RACE_ITEMLESS_FFA]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_ITEMLESS_DUOS]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_ITEMLESS_3V3]: { rank: Number, position: Number, lastActivity: Number },
+  [RACE_ITEMLESS_4V4]: { rank: Number, position: Number, lastActivity: Number },
+  [BATTLE_1V1]: { rank: Number, position: Number, lastActivity: Number },
   [BATTLE_FFA]: { rank: Number, position: Number, lastActivity: Number },
+  [BATTLE_DUOS]: { rank: Number, position: Number, lastActivity: Number },
+  [BATTLE_3V3]: { rank: Number, position: Number, lastActivity: Number },
+  [BATTLE_4V4]: { rank: Number, position: Number, lastActivity: Number },
+  [BATTLE_SURVIVAL]: { rank: Number, position: Number, lastActivity: Number },
 });
 
 module.exports.Rank = model('rank', Rank);
